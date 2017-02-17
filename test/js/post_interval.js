013d4493a866dd72e16948831515b450f7f6f9d8
honkeechan <honkeechan@hotmail.com>
2017-02-17 08:57:48 +0800

    write data post success

--- test/js/post_interval.js
+++ test/js/post_interval.js
@@ -5,47 +5,47 @@
 var http = require('http');
 var timeTools = require("../../lib/timetools");
 
-var qs = require('querystring');
+function postMethod(){
+    var qs = require('querystring');
+    var nowDateTime = timeTools.nowDateTime();
+    var post_data = {
+        a: 123,
+        time: nowDateTime
+    };//这是需要提交的数据
 
-var post_data = {
-    a: 123,
-    time: new Date().getTime()};//这是需要提交的数据
+    var content = qs.stringify(post_data);
 
 
-var content = qs.stringify(post_data);
+    var options = {
+        hostname: 'honkeechan.cn',
+        port: 3008,
+        path: '/write_data',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+    };
 
-var nowDateTime = timeTools.nowDateTime();
-var options = {
-    hostname: 'honkeechan.cn',
-    port: 3008,
-    path: '/write_data',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-    }
-};
-
-var req = http.request(options, function (res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-        console.log('BODY: ' + chunk);
+    var req = http.request(options, function (res) {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+        });
     });
-});
 
-req.on('error', function (e) {
-    console.log('problem with request: ' + e.message);
-});
+    req.on('error', function (e) {
+        console.log('problem with request: ' + e.message);
+    });
 
 // write data to request body
-req.write(content);
+    req.write(content);
+    req.end();
+}
 
-req.end();
 
-window.setInterval(function(){
-    if(videoTitle != null && videoTitle != page.title){
-        //console.log(videoTitle);
-        phantom.exit();
-    }
+
+setInterval(function(){
+    postMethod();
 }, 1000);
--- test/js/visit_website_interval.js
+++ test/js/visit_website_interval.js
@@ -5,19 +5,11 @@
 var page = require('webpage').create();
 // video url must have http:// suffix
 //var videoUrl = phantom.args[0];
-var videoUrl = "http://honkeechan.cn:3008/write_data";
-var videoTitle = null;
-var playVideo = null;
-window.setTimeout(function(){
-    phantom.exit();
-},  3* 60 * 60 * 1000);
+var videoUrl = "http://127.0.0.1:3008/write_data";
+page.open(videoUrl , function () {
+    window.setInterval(function(){
+        page.reload();
+    }, 10000);
+});
 
-//it is a timer for the same page.
-//if it redirect to other video page because this video has finish
-// we will kill the browser.
-window.setInterval(function(){
-    if(videoTitle != null && videoTitle != page.title){
-        //console.log(videoTitle);
-        phantom.exit();
-    }
-}, 1000);
+
